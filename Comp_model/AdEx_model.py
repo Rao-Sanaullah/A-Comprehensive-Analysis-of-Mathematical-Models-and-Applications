03fbb3d731b5827b0de6de0da9d2cf7f02ead17e
Sanaullah <mr.raosanaullah@gmail.com>
2024-03-01 15:15:57 +0100

    Update AdEx_model.py

--- Comp_model/AdEx_model.py
+++ Comp_model/AdEx_model.py
@@ -9,7 +9,7 @@ class AdEX:
         self.delta_T = delta_T
         self.v_reset = v_reset
         self.v = v_init
-        self.num_ops = 5  # one multiplication, four additions
+        self.num_ops = 5  
         
     def update(self, I, dt):
         dvdt = (-self.v + self.tau_m * I - self.v_rheo + self.delta_T * np.exp((self.v - self.v_spike) / self.delta_T)) / self.tau_m
@@ -17,8 +17,9 @@ class AdEX:
         spike = self.v >= self.v_spike
         if spike:
             self.v = self.v_reset
-            self.num_ops += 4  # two multiplications, two additions
+            self.num_ops += 4 
         else:
-            self.num_ops += 1  # one multiplication
-        self.num_ops += 3  # three additions
+            self.num_ops += 1 
+        self.num_ops += 3  
         return spike
+
