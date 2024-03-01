a34905f4f897058532578f49de0da1260dc8c00c
Sanaullah <mr.raosanaullah@gmail.com>
2024-03-01 15:15:05 +0100

    Update IFSFA_model.py

--- Comp_model/IFSFA_model.py
+++ Comp_model/IFSFA_model.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# define IFSFA model
 class IFSFA:
     def __init__(self, tau_m, tau_w, a, b, delta_T, v_reset, v_th, v_init=0.0):
         self.tau_m = tau_m
@@ -11,19 +10,21 @@ class IFSFA:
         self.v_reset = v_reset
         self.v_th = v_th
         self.v = v_init
+        self.w = 0.0
+        self.weight = np.random.normal(loc=0.0, scale=1.0)
         self.num_ops = 0
-        
+
     def update(self, I, dt):
-        dwdt = (self.a*(self.v-self.v_reset)-self.w) / self.tau_w
+        dwdt = (self.a * (self.v - self.v_reset) - self.w) / self.tau_w
         self.w += dwdt * dt
-        dvdt = (-self.v + self.delta_T*np.exp((self.v-self.v_th)/self.delta_T) + np.dot(self.weights.T, self.w) + I) / self.tau_m
+        dvdt = (-self.v + self.delta_T * np.exp((self.v - self.v_th) / self.delta_T) + self.weight * self.w + I) / self.tau_m
         self.v += dvdt * dt
-        self.num_ops += 2  # increment number of operations for the two derivative calculations above
         if self.v >= self.v_th:
             spike = True
             self.v = self.v_reset
             self.w += self.b
-            self.num_ops += 2  # increment number of operations for the reset and synaptic update
+            self.num_ops += 4 
         else:
             spike = False
+            self.num_ops += 2  
         return spike
