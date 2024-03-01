62fcb04e06393324db3375d1ab2e5ee9fe33f578
Sanaullah <mr.raosanaullah@gmail.com>
2024-03-01 15:13:54 +0100

    Update SRM_model.py

--- Comp_model/SRM_model.py
+++ Comp_model/SRM_model.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-# define SRM model
 class SRM:
     def __init__(self, tau_s, tau_r, v_reset, v_th, v_init=0.0):
         self.tau_s = tau_s
@@ -8,25 +5,28 @@ class SRM:
         self.v_reset = v_reset
         self.v_th = v_th
         self.v = v_init
-        self.num_ops = 3 # three additions
-    
+        self.s = 0.0
+        self.r = 0.0
+        self.weight = np.random.normal(loc=0.0, scale=1.0)
+        self.num_ops = 3  
+
     def update(self, I, dt):
         dsdt = -self.s / self.tau_s + self.r
         drdt = -self.r / self.tau_r
         self.s += dsdt * dt
         self.r += drdt * dt
-        dvdt = (-self.v + np.dot(self.weights.T, self.s) + I) / self.tau_s
+        dvdt = (-self.v + self.weight * self.s + I) / self.tau_s
         self.v += dvdt * dt
-        self.num_ops += 3 # three additions
-        
+        self.num_ops += 3  
+
         if self.v >= self.v_th:
             spike = True
             self.v = self.v_reset
             self.s += 1.0
             self.r += 1.0
-            self.num_ops += 5 # two additions, two divisions, one multiplication
+            self.num_ops += 5
         else:
             spike = False
-            self.num_ops += 1 # one comparison
-        
+            self.num_ops += 1  # one comparison
+
         return spike
