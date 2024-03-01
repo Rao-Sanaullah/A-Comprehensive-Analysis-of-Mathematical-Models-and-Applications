7481907321f5446d60f31b6513ac65cc1f650aed
Sanaullah <mr.raosanaullah@gmail.com>
2024-03-01 15:14:27 +0100

    Update ThetaNeuron_model.py

--- Comp_model/ThetaNeuron_model.py
+++ Comp_model/ThetaNeuron_model.py
@@ -1,25 +1,25 @@
 import numpy as np
 
-# define ThetaNeuron model
 class ThetaNeuron:
     def __init__(self, tau, v_reset, v_th, v_init=0.0):
         self.tau = tau
         self.v_reset = v_reset
         self.v_th = v_th
         self.v = v_init
-        self.num_ops = 0  # initialize the number of operations to 0
-
+        self.theta = np.random.uniform(low=0, high=2 * np.pi)
+        self.weight = np.random.normal(loc=0.0, scale=1.0)
+        self.num_ops = 0 
     def update(self, I, dt):
         dthetadt = 1.0 - np.cos(self.theta)
-        dvdt = (-self.v + I + np.dot(self.weights.T, dthetadt)) / self.tau
+        dvdt = (-self.v + I + self.weight * dthetadt) / self.tau
         self.v += dvdt * dt
         self.theta += 0.05 * 2 * np.pi * dt  # theta frequency is 10 Hz
         self.theta %= 2 * np.pi
-        self.num_ops += 3  # increment the number of operations by 3 for each update (2 multiplications and 1 addition)
+        self.num_ops += 3  
         if self.v >= self.v_th:
             spike = True
             self.v = self.v_reset
-            self.num_ops += 1  # increment the number of operations by 1 for the spike
+            self.num_ops += 1  
         else:
             spike = False
         return spike
