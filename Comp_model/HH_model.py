5bd6d077a8852bad5745c68e12113868c8fde610
Sanaullah <mr.raosanaullah@gmail.com>
2024-03-05 12:06:35 +0100

    Update HH_model.py

--- Comp_model/HH_model.py
+++ Comp_model/HH_model.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# define HH model
 class HH:
     def __init__(self, v_init=-65.0, n_init=0.3177, m_init=0.0529, h_init=0.5961):
         self.v = v_init
@@ -14,7 +13,7 @@ class HH:
         self.E_Na = 50.0
         self.E_K = -77.0
         self.E_L = -54.4
-        self.num_ops = 18  # initial number of operations
+        self.num_ops = 0  # initial number of operations
         
     def update(self, I, dt):
         alpha_n = (0.01 * (self.v + 55.0)) / (1.0 - np.exp(-0.1 * (self.v + 55.0)))
@@ -24,25 +23,37 @@ class HH:
         alpha_h = 0.07 * np.exp(-0.05 * (self.v + 65.0))
         beta_h = 1.0 / (1.0 + np.exp(-0.1 * (self.v + 35.0)))
         
-        dvdt = (I - self.g_Na * self.m**3 * self.h * (self.v - self.E_Na) - self.g_K * self.n**4 * (self.v - self.E_K) - self.g_L * (self.v - self.E_L)) / self.C
-        dndt = alpha_n * (1.0 - self.n) - beta_n * self.n
-        dmdt = alpha_m * (1.0 - self.m) - beta_m * self.m
-        dhdt = alpha_h * (1.0 - self.h) - beta_h * self.h
+        # Intermediate computations
+        n_inf = alpha_n / (alpha_n + beta_n)
+        m_inf = alpha_m / (alpha_m + beta_m)
+        h_inf = alpha_h / (alpha_h + beta_h)
         
+        # Update gating variables
+        self.n += (n_inf - self.n) * dt
+        self.m += (m_inf - self.m) * dt
+        self.h += (h_inf - self.h) * dt
+        
+        # Currents
+        ina = self.g_Na * self.m**3 * self.h * (self.v - self.E_Na)
+        ik = self.g_K * self.n**4 * (self.v - self.E_K)
+        il = self.g_L * (self.v - self.E_L)
+        
+        # Update membrane potential
+        dvdt = (I - ina - ik - il) / self.C
         self.v += dvdt * dt
-        self.n += dndt * dt
-        self.m += dmdt * dt
-        self.h += dhdt * dt
         
-        self.num_ops = 18 + 15  # number of operations for the above lines
+        # Increment number of operations
+        self.num_ops += 6  # Number of operations for intermediate computations
         
-        spike = self.v >= 0.0
+        # Check for spike
+        spike = self.v >= -65.0
         if spike:
             self.v = 30.0
             self.n = 0.3177
             self.m = 0.0529
             self.h = 0.5961
-            self.num_ops += 32  # number of operations for the above lines
-        self.num_ops += 12  # number of operations for the if condition and return statement
+            self.num_ops += 24  # Number of operations for resetting variables
+        
+        self.num_ops += 14  # Number of operations for other calculations and conditions
         
         return spike
